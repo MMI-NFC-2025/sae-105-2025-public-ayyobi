32f678c9f92686324a8f18b41795cc43aa52db4b
Nargis <125502580+ayyobi@users.noreply.github.com>
2025-12-12 01:06:54 +0100

    page artiste template style css

--- src/js/search.js
+++ src/js/search.js
@@ -1,6 +1,1 @@
 // Recherche interne / focus sur formulaire (placeholder)
-export function attachSearch(inputSelector){
-  const input=document.querySelector(inputSelector);
-  if(!input) return;
-  input.addEventListener('keyup',()=>{/* recherche basique */});
-}
